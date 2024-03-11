7bd4ca745846f17fd81a9fb260c65e272157a33b
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-03-11 05:52:11 -0700

    first basic reactDOM.render examples written

---
+++ index.js
@@ -0,0 +1,5 @@
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+ReactDOM.render(<p>Hi my name is Kristijan!</p>, document.getElementById("root"));
+ReactDOM.render(
+    <ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>, 
+    document.getElementById("root"));
