355be150d6a8bf33e1235874158940a81cceac7b
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-03-15 05:21:35 -0700

    React info site finished, css styles added & project fully implemented in Vite

---
+++ Scrimba-Build a React info site/scrimba React info site/src/App.jsx
@@ -0,0 +1,11 @@
+import Main from './Main.jsx'
+import Navbar from './Navbar.jsx'
+
+export default function App(){
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div> 
+    )
+}
---
+++ Scrimba-Build a React info site/scrimba React info site/src/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return(
+        <nav>
+            <img src="react-icon-small.png" className="nav--icon"></img>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
