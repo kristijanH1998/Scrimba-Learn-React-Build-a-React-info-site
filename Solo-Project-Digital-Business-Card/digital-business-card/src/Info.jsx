9d029e198b886c848fe08dbf2db3c8b2af9cf955
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-03-20 02:36:42 -0700

    all components' design finished, personal information content added to all parts of the card; profile picture put on top of the card

--- Solo-Project-Digital-Business-Card/digital-business-card/src/Info.jsx
+++ Solo-Project-Digital-Business-Card/digital-business-card/src/Info.jsx
@@ -3,11 +3,17 @@ export default function Info() {
         <>
             <img src="./profile-pic.jpg" className="image"></img>
             <h1>Kristijan Hornung</h1>
-            <h3>Software Developer</h3>
+            <h3 id="profession">Software Developer</h3>
             <h5>kristijanH1998.github.io</h5>
             <div className="buttons">
-                <button>Email</button>
-                <button>LinkedIn</button>
+                <div id="email">
+                    <i className="fa-solid fa-2x fa-envelope icon-button--icon"></i>
+                    <span className="icon-button--text">Email</span>
+                </div>
+                <div id="linkedin">
+                    <i className="fa fa-2x fa-linkedin" aria-hidden="true"></i>
+                    <span className="icon-button--text">LinkedIn</span>
+                </div>
             </div>
         </>
     )
